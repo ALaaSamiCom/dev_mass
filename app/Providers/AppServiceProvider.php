63a054a488845e096a97d6ded8ad7e54f0df8447
ALaa Sami <alaasamicom@gmail.com>
2022-12-19 09:31:31 +0200

    updated done

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,12 +27,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-//        $links = SocialLink::all();
-//        $setting = Setting::first();
-//        $headerpages = SpecialSection::all();
-//        view()->share('links',$links);
-//        view()->share('setting',$setting);
-//        view()->share('headerpages',$headerpages);
+        $links = SocialLink::all();
+        $setting = Setting::first();
+        $headerpages = SpecialSection::all();
+        view()->share('links',$links);
+        view()->share('setting',$setting);
+        view()->share('headerpages',$headerpages);
 
     }
 }
