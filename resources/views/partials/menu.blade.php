bd28afb807236d4cfe63dfc490d5161620e51348
ALaa Sami <alaasamicom@gmail.com>
2022-12-19 13:45:22 +0200

    add setting to direct link on admin menu

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -480,7 +480,7 @@
                 <ul class="c-sidebar-nav-dropdown-items">
                     @can('setting_access')
                         <li class="c-sidebar-nav-item">
-                            <a href="{{ route("admin.settings.index") }}" class="c-sidebar-nav-link {{ request()->is("admin/settings") || request()->is("admin/settings/*") ? "c-active" : "" }}">
+                            <a href="{{ route("admin.settings.edit",1) }}" class="c-sidebar-nav-link {{ request()->is("admin/settings") || request()->is("admin/settings/*") ? "c-active" : "" }}">
                                 <i class="fa-fw fas fa-cogs c-sidebar-nav-icon">
 
                                 </i>
